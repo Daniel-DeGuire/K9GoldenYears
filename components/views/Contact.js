27447858903c68211b012cac2d9b008b07282ed6
Daniel DeGuire <danjdeguire@gmail.com>
2021-10-12 17:52:56 -0500

    add responsive google maps

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -26,7 +26,15 @@ method="POST"
 <!-- your other form fields go here -->
 <button type="submit">Send</button>
 </form></div>
-    </div>
+    </div><br><br>
+    <h2>Additional contact information</h2>
+    <p>K9 Golden Years Foundation<br>
+    1600 South Brentwood Ave.<br>
+    Brentwood, Missouri 63144 <br>
+    </p>
+    <div class="maps">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12467.907131529033!2d-90.34586666606317!3d38.62641516634893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d8cb19fab5409d%3A0x743a403254ca3db!2s1600%20S%20Brentwood%20Blvd%2C%20Brentwood%2C%20MO%2063144!5e0!3m2!1sen!2sus!4v1634005156239!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div>
+    <p> </p><br>
 </div>
 </div>
 </section>
