0ed37789d6639ed91853a305c0bcbafff741efc7
Daniel DeGuire <danjdeguire@gmail.com>
2021-10-11 20:15:57 -0500

    update CSS stying

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,3 @@
 export default () => `
-<footer> <img src="https://cdn.pixabay.com/photo/2021/06/15/12/28/tiktok-6338430_960_720.png" width="50px"> </footer>
+<footer> <img src="https://cdn.pixabay.com/photo/2021/06/15/12/28/tiktok-6338430_960_720.png" width="50px"><h2>(c)2021 Golden Years Holdings Company</h2></footer>
 `;
