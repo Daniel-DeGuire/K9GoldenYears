c223ac96b069d1ed7c4f5d0475da43b6bc483c82
Daniel DeGuire <danjdeguire@gmail.com>
2021-10-28 21:16:52 -0500

    Organize code

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,4 +1,7 @@
-export default (st) => `
+import html from "html-literal";
+import dogsImage from "/assets/dog2.jpg";
+
+export default (st) => html`
 <section id="home">
 <div id="container">
 <div id="header">
@@ -12,8 +15,9 @@ export default (st) => `
           <div class="contenttext">
           <h2>Mission:</h2>
           <P> Providing educational & community resources in adopting a senior dogs. Senior dogs have a much lower adoption rate versus younger dogs & puppies. We strive to change this one dog & heart at a time.  <br>
-          <br>
-          <br>
+          <br><img src="${dogsImage}"/>
+          <br><b>Resident dogs Indie (left) and Scarlett (right)</b></P>
+          <br><br>
           <h2>Random Dog Facts</h2>
           <P>Interesting dog facts.</P>
           <br>
@@ -26,9 +30,8 @@ export default (st) => `
           Visitor number
           ${st.visits}
           </table></p>
-
-          </div>
-      </div>
+        </div><BR>
+        </div>
 </table>
 </section>
 `;
